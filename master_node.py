f18e1b4ba918e4b78e0d33af09b0cb4f9b06cf3e
an1t6 <rkdrlgus725@gmail.com>
2024-09-24 02:49:31 +0900

    done

--- master_node.py
+++ master_node.py
@@ -4,25 +4,25 @@ import logging
 import pickle
 import socket
 import threading
+from queue import Queue 
 
 SIZE = 10
-WOKERS = 4
-DELAY = {'min': 1, 'max': 3}  
-ONE_SEC_DELAY = 1 
+WORKERS = 4
+DELAY = {'min': 1, 'max': 3} 
+ONE_SEC_DELAY = 1
 PORT = 8081
 
-def set_logging(filename):
-    logger = logging.getLogger(filename)
-    handler = logging.FileHandler(filename, mode='w', encoding='utf-8')
+def set_logging(file):
+    logger = logging.getLogger(file)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler(file, mode='w', encoding='utf-8')
+    logger.addHandler(handler)
     formatter = logging.Formatter('%(asctime)s - %(message)s')
     handler.setFormatter(formatter)
-    logger.setLevel(logging.INFO)
-    logger.addHandler(handler)
     return logger
 
-def log_print(logger, message):
-    logger.info(message)
-    print(message)
+def log_print(log, text):
+    log.info(text)
 
 def random_sec():
     return random.randint(DELAY['min'], DELAY['max'])
@@ -30,80 +30,86 @@ def random_sec():
 def create_matrix(size):
     return [[random.randint(0, 99) for _ in range(size)] for _ in range(size)]
 
-def manage_worker(worker_socket, worker_num, temps, Mat1, Mat2, result, temp_counts, total_times, lock, worker_loggers, master_logger):
-    while temps:
+def manage_worker(worker_socket, worker_num, temps_queue, Mat1, Mat2, result, count, total_times, lock, worker_log, master_log):
+    while not temps_queue.empty(): 
+        temp = None
         with lock:
-            if temps:
-                temp = temps.pop(0)
+            if not temps_queue.empty():
+                temp = temps_queue.get()  
             else:
                 break
-        
+        if temp is None:
+            break
+
         temp_data = {'temp': temp, 'Mat1': Mat1, 'Mat2': Mat2}
         worker_socket.sendall(pickle.dumps(temp_data))
         worker_response = pickle.loads(worker_socket.recv(4096))
         success = worker_response['success']
         temp_result = worker_response['result']
         row, col = temp['row'], temp['col']
-        time_taken = worker_response['time_taken']
-        temp_description = f"[{row}, {col}]"
-        logger = worker_loggers[worker_num]
-        
-        log_print(logger, 
-                    f"작업 {temp_description}을 완료, 소요된 시간 {time_taken}초")
+        run_time = worker_response['run_time']
+        current_location = f"[{row}, {col}]"
+        current_result = 0
+        worker_logger = worker_log[worker_num]
+       
+        for k in range(SIZE):
+            current_result += Mat1[row][k] * Mat2[k][col]
         with lock:
             if success:
                 result[row][col] = temp_result
-                temp_counts[worker_num] += 1
-                total_times[worker_num] += time_taken
+                count[worker_num] += 1
+                total_times[worker_num] += run_time
+                log_print(master_log, f'Worker {worker_num + 1} : {current_location} = {current_result}')
+                log_print(worker_logger, f'연산 성공 : {current_location}, 소요된 시간 {run_time}초')
+                print(f'작업 {current_location} 성공')
             else:
-                temps.append(temp)
-                log_print(master_logger, f'워커 {worker_num + 1}이 행렬 [{temp["row"]}, {temp["col"]}] 처리를 실패, 재할당')
-        
+                temps_queue.put(temp)
+                log_print(master_log, f'Worker {worker_num + 1} : {current_location} 연산 실패, 다른 워커로 재할당. . .')
+                log_print(worker_logger, f'연산 실패 : {current_location}')
+                print(f'작업 {current_location} 실패')
         time.sleep(ONE_SEC_DELAY)
         
 def main():
-    
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('ec2-43-203-247-248.ap-northeast-2.compute.amazonaws.com', PORT))
-    server_socket.listen(WOKERS)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('127.0.0.1', PORT))
+    server.listen(WORKERS)
     
     Mat1 = create_matrix(SIZE)
     Mat2 = create_matrix(SIZE)
     result = [[0] * SIZE for _ in range(SIZE)]
-    temps = [{'row': i, 'col': j} for i in range(SIZE) for j in range(SIZE)]
-    temp_counts = [0] * WOKERS
+    temps_queue = Queue()
+    for i in range(SIZE):
+        for j in range(SIZE):
+            temps_queue.put({'row': i, 'col': j})
+    count = [0] * WORKERS
     start_time = time.time()
-    total_times = [0] * WOKERS
+    total_times = [0] * WORKERS
     worker_threads = []
+    master_log = set_logging('master.txt')
+    worker_log = [set_logging(f'worker{i + 1}.txt') for i in range(WORKERS)]
     lock = threading.Lock()
     
-    master_logger = set_logging('master.txt')
-    worker_loggers = [set_logging(f'worker{i + 1}.txt') for i in range(WOKERS)]
     print(f'워커의 접속을 기다리는 중...')
-
-    for worker_num in range(WOKERS):
-        worker_socket, addr = server_socket.accept()
-        log_print(master_logger, f'워커 {worker_num + 1}이 연결됨 {addr}')
+    for worker_num in range(WORKERS):
+        worker_socket, addr = server.accept()
+        log_print(master_log, f'워커 {worker_num + 1} 연결됨 {addr}')
         print(f'워커 {worker_num + 1} 연결 성공 {addr}')
-        t = threading.Thread(target=manage_worker, args=(worker_socket, worker_num, temps, Mat1, Mat2, result, temp_counts, total_times, lock, worker_loggers, master_logger))
+        t = threading.Thread(target=manage_worker, args=(worker_socket, worker_num, temps_queue, Mat1, Mat2, result, count, total_times, lock, worker_log, master_log))
         t.start()
         worker_threads.append(t)
         
-    log_print(master_logger, f'{SIZE}x{SIZE} 크기의 행렬 2개를 {WOKERS}개의 워커로 연산 시작')
-    
+    log_print(master_log, f'{SIZE}x{SIZE} 크기의 행렬 2개를 {WORKERS}개의 워커로 연산 시작')
     for t in worker_threads:
         t.join()
 
-    total_complete = sum(temp_counts)
-    total_time = time.time() - start_time
-
-    log_print(master_logger, f'총 작업 수 {total_complete}')
-    log_print(master_logger, f'총 연산 수행 시간 {total_time} 초')
-    for i in range(WOKERS):
-        log_print(worker_loggers[i], f'worker {i + 1}이 처리한 작업 수 {temp_counts[i]}')
-        log_print(worker_loggers[i], f'worker {i + 1}이 수행한 시간 {total_times[i]} 초')
-
-    server_socket.close()
+    total_complete = sum(count)
+    total_time = round(time.time() - start_time)
+    log_print(master_log, f'총 작업 수 {total_complete}')
+    log_print(master_log, f'총 연산 수행 시간 {total_time} 초')
+    for i in range(WORKERS):
+        log_print(worker_log[i], f'worker {i + 1}이 처리한 작업 수 {count[i]}')
+        log_print(worker_log[i], f'worker {i + 1}이 수행한 시간 {total_times[i]} 초')
+    server.close()
 
 if __name__ == '__main__':
     main()
--- worker_node.py
+++ worker_node.py
@@ -1,11 +1,11 @@
-import socket
-import threading
-import pickle
 import random
 import time
+import pickle
+import socket
+import threading
 
 SIZE = 10
-DELAY = {'min': 1, 'max': 3}  # 1~3초 랜덤 딜레이
+DELAY = {'min': 1, 'max': 3}
 PORT = 8081
 
 def random_sec():
@@ -13,22 +13,18 @@ def random_sec():
 
 def run_temp(temp, Mat1, Mat2):
     row, col = temp['row'], temp['col']
-    row_data = Mat1[row]
-    col_data = [Mat2[i][col] for i in range(SIZE)]
-    temp_result = sum(row_data[i] * col_data[i] for i in range(SIZE))
-    time_taken = random_sec()
-    time.sleep(time_taken)
+    temp_row = Mat1[row]
+    temp_col = [Mat2[i][col] for i in range(SIZE)]
+    temp_result = sum(temp_row[i] * temp_col[i] for i in range(SIZE))
+    run_time = random_sec()
+    time.sleep(run_time)
     success = random.random() < 0.8
-    return {
-        'success': success,
-        'result': temp_result,
-        'time_taken': time_taken
-    }
+    return { 'success': success, 'result': temp_result,'run_time': run_time }
     
-def worker_thread(worker_id):
+def worker_thread(worker_num):
     
     worker_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    worker_socket.connect(('ec2-43-203-247-248.ap-northeast-2.compute.amazonaws.com', PORT))
+    worker_socket.connect(('127.0.0.1', PORT))
 
     while True:
         temp_data = pickle.loads(worker_socket.recv(4096))
@@ -43,8 +39,8 @@ def worker_thread(worker_id):
 
 def main():
     threads = []
-    for worker_id in range(4):
-        t = threading.Thread(target=worker_thread, args=(worker_id,))
+    for worker_num in range(4):
+        t = threading.Thread(target=worker_thread, args=(worker_num,))
         t.start()
         threads.append(t)
     for t in threads:
